27e9c3ea3cfe0cfb1eb14244f831742dcd841c07
AlitPurnama <alitpurnama13@gmail.com>
2026-05-10 03:34:21 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://alitpurnama.github.io",
+  base: "alitpurnama.github.io",
   image: {
     service: passthroughImageService(),
   },
