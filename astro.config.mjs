5712fea71a64429f8ef5015c46eb4b00fca06516
AlitPurnama <alitpurnama13@gmail.com>
2026-05-10 04:51:46 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, passthroughImageService } from "astro/config";
 
 import mdx from "@astrojs/mdx";
-
+const isProd = process.env.NODE_ENV === "production";
 // https://astro.build/config
 export default defineConfig({
-  site: "https://alitpurnama.github.io",
-  base: "alitpurnama.github.io",
+  site: isProd ? "https://alitpurnama.github.io" : "http://localhost:4321",
+  base: isProd ? "alitpurnama.github.io" : undefined,
   image: {
     service: passthroughImageService(),
   },
